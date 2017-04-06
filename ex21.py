3b4c23d72cf1fdd47e87c4a9ca9e0f3000cc3b39
DLTarasi <dltarasi@gmail.com>
2017-04-06 14:20:59 -0600

    ex 21 drills

--- ex21.py
+++ ex21.py
@@ -29,5 +29,17 @@ print(f"Age: {age}, Height: {height}, Weight, {weight}, IQ: {iq}")
 print("Here is a puzzle.")
 
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+# Breaking this down action by action:
+# add(age, subtract(height, multiply(weight, divide(50, 2))))
+# add(age, subtract(height, multiply(weight, 25)))
+# add(age, subtract(height, multiply(180, 25)))
+# add(age, subtract(height, multiply(180, 25)))
+# add(age, subtract(height, 4500))
+# add(age, subtract(74, 4500))
+# add(age, -4426)
+# add(35, -4426)
+#-4391
+
+print(35 + (74 - (180 * (50 / 2))))
 
 print("That becomes: ", what, "Can you do it by hand?")
---
+++ test.py
@@ -0,0 +1,5 @@
+def add(a, b):
+    print(f"ADDING {a} + {b}")
+    return a + b
+
+age = add(30, 5)
