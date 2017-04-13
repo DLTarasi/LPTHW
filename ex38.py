23d67b908c770697dad301fed01cfef24bd95523
DLTarasi <dltarasi@gmail.com>
2017-04-13 16:23:50 -0600

    ex39

--- ex38.py
+++ ex38.py
@@ -1,3 +1,9 @@
+"""
+testing
+to see
+if this works
+for comments"""
+
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 print("Wait there are not 10 things in that list. Let's fix that.")
